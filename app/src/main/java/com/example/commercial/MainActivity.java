c5a444fc8f97032a09d0928ec019485862342004
Enigma <sabuhiakbarli@gmail.com>
2022-07-08 13:57:59 +0400

    # Fragment test

--- app/src/main/java/com/example/commercial/MainActivity.java
+++ app/src/main/java/com/example/commercial/MainActivity.java
@@ -2,16 +2,19 @@ package com.example.commercial;
 
 
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 
 import androidx.cardview.widget.CardView;
@@ -27,29 +30,24 @@ import com.example.commercial.databinding.ActivityMainBinding;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class MainActivity extends AppCompatActivity  {
-    ActivityMainBinding binding;
+
     LinearLayoutManager mLinearLayoutManager;
     RecyclerView mRecyclerView;
     FirebaseDatabase mFirebaseDatabase;
     DatabaseReference mDatabaseReference;
     FirebaseRecyclerOptions<Product> options;
     FirebaseRecyclerAdapter<Product , ViewHolder> firebaseRecyclerAdapter;
-
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout , fragment);
-        fragmentTransaction.commit();
-    }
+    BottomNavigationView bottomNavigationView;
+    ActivityMainBinding binding;
 
 
   CardView b1back , b2back , b3back , b4back;
@@ -57,29 +55,11 @@ public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG,"onCreate");
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        replaceFragment(new FirstFragment());
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-                    switch (item.getItemId()) {
-                        case R.id.firstFragment:
-                            replaceFragment(new FirstFragment());
-                            break;
-                        case R.id.secondFragment:
-                            replaceFragment(new SecondFragment());
-                            break;
-                        case R.id.thirdFragment:
-                            replaceFragment(new ThirdFragment());
-                            break;
-                        case R.id.fourthFragment:
-                            replaceFragment(new FourthFragment());
-                            break;
-                    }
-            return true;
-        });
+        setContentView(R.layout.activity_main);
 
-        mLinearLayoutManager = new LinearLayoutManager(this);
+  mLinearLayoutManager = new LinearLayoutManager(this);
         mLinearLayoutManager.setReverseLayout(true);
         mLinearLayoutManager.setStackFromEnd(true);
 
@@ -88,7 +68,7 @@ public class MainActivity extends AppCompatActivity  {
         mDatabaseReference = mFirebaseDatabase.getReference("Products");
         showData();
 
-        b2back = findViewById(R.id.b2back);
+      /*  b2back = findViewById(R.id.b2back);
         b2back.setFocusable(true);
         b2back.setFocusableInTouchMode(true);
         b2back.requestFocus();
@@ -97,12 +77,36 @@ public class MainActivity extends AppCompatActivity  {
         b1back = findViewById(R.id.b1back);
         b1back.setFocusable(true);
         b1back.setFocusableInTouchMode(true);
-        b1back.requestFocus();
+        b1back.requestFocus();*/
 
-
-
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+       getSupportFragmentManager().beginTransaction().replace(R.id.main_container , new FirstFragment()).commit();
+      bottomNavigationView.setSelectedItemId(R.id.home);
+      bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+          @Override
+          public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+              Fragment fragment = null;
+             switch (item.getItemId()) {
+                 case R.id.home:
+                     fragment = new FirstFragment();
+                     break;
+                 case R.id.favorite:
+                     fragment = new SecondFragment();
+                     break;
+                 case R.id.cart:
+                     fragment = new ThirdFragment();
+                     break;
+                 case R.id.profile:
+                     fragment = new FourthFragment();
+                     break;
+             }
+              getSupportFragmentManager().beginTransaction().replace(R.id.main_container ,fragment).commit();
+              return true;
+          }
+      });
 
     }
+
     private void showData() {
         options = new FirebaseRecyclerOptions.Builder<Product>().setQuery(mDatabaseReference , Product.class).build();
         firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Product, ViewHolder>(options) {
@@ -119,18 +123,18 @@ public class MainActivity extends AppCompatActivity  {
                   viewHolder.setOnClickListener(new ViewHolder.ClickListener() {
                       @Override
                       public void onItemClick(View view, int position) {
-                          Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+                         // Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
                       }
 
                       @Override
                       public void onItemLongClick(View view, int position) {
-                          Toast.makeText(MainActivity.this, "Long Click", Toast.LENGTH_SHORT).show();
+                         // Toast.makeText(MainActivity.this, "Long Click", Toast.LENGTH_SHORT).show();
                       }
                   });
                 return  viewHolder;
             }
         };
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL, false);
+       GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(gridLayoutManager);
         firebaseRecyclerAdapter.startListening();
         mRecyclerView.setAdapter(firebaseRecyclerAdapter);
