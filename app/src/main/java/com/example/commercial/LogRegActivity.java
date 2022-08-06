bf893e4db5583dc980cab50ed77fcfad469d1052
Enigma <sabuhiakbarli@gmail.com>
2022-08-06 23:40:45 +0400

    #Activity for Sign in and sign up fragment

---
+++ app/src/main/java/com/example/commercial/LogRegActivity.java
@@ -0,0 +1,14 @@
+package com.example.commercial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogRegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_reg);
+    }
+}
